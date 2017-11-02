d5796fb08775bdf5de13aa940a0cda3658f4cf78
Ega Radiegtya <radiegtya@Egas-MacBook-Pro.local>
2017-11-03 05:44:54 +0700

    2.day2-state

--- App.js
+++ App.js
@@ -1,25 +1,52 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
-
-import Header from './components/Header';
-import Content from './components/Content';
+import {View, Text, StyleSheet, TouchableOpacity, TextInput} from 'react-native';
 
 export default class App extends Component{
 
-  render(){
-    const description = "Lorem Ipsum dolor sit amet";
+  state = {
+    startups: ["tokopedia", "bukalapak", "dumbways"],
+    startup: ""
+  }
 
+  renderRow(startup, index){
     return (
-      <View>
-        <Header
-          title="Kursus React"
-          color="#FFF"
-        />
-        <Content backgroundColor="#8CC570">
-          <Text>{description}</Text>
-        </Content>
+      <View key={index} style={styles.listItem}>
+        <Text>{startup}</Text>
       </View>
     );
   }
 
+  handleAdd(){
+    let newStartups = this.state.startups;
+    newStartups.push(this.state.startup);
+    this.setState({
+      startups: newStartups,
+      startup: ""
+    })
+  }
+
+  render(){
+    return (
+      <View>
+        {this.state.startups.map((startup, index)=> this.renderRow(startup, index))}
+
+        <TextInput
+           style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+           onChangeText={(startup) => this.setState({startup})}
+           value={this.state.startup}
+         />
+        <TouchableOpacity onPress={()=> this.handleAdd()}>
+          <Text>Add To List</Text>
+        </TouchableOpacity>
+      </View>
+    )
+  }
+
 }
+
+const styles = StyleSheet.create({
+  listItem: {
+    padding: 10,
+    borderBottomWidth: 0.5
+  }
+});
