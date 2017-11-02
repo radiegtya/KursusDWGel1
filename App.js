d2c5f0e396539d0b420e70868dc7a4f5bdd50fe6
Ega Radiegtya <radiegtya@Egas-MacBook-Pro.local>
2017-11-03 06:14:09 +0700

    3.day2-flexbox

--- App.js
+++ App.js
@@ -1,35 +1,45 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 
 export default class App extends Component{
 
-  constructor(){
-    super();
-    console.log("1. Constructor");
-  }
-
-  componentWillMount(){
-    console.log("2. componentWillMount");
-  }
-
-  componentDidMount(){
-    console.log("3. componentDidMount");
-  }
-
-  componentWillReceiveProps(){
-    console.log("componentWillReceiveProps");
-  }
-
-  componentWillUnmount(){
-    console.log("4. componentWillUnmount");
-  }
-
   render(){
     return (
-      <View>
-        <Text>App.js</Text>
+      <View style={styles.container}>
+
+        {/* flex 2 */}
+        <View style={styles.firstSide}>
+          <Text>First Side</Text>
+        </View>
+
+        {/* flex 1 */}
+        <View style={styles.secondSide}>
+          <Text>Second Side</Text>
+        </View>
+
       </View>
     )
   }
 
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    flexDirection: 'column'
+  },
+  firstSide: {
+    flex: 0.2,
+    backgroundColor: 'red',
+    padding: 20,
+    justifyContent: 'center',
+    alignSelf: 'center'
+  },
+  secondSide: {
+    flex: 7,
+    backgroundColor: 'yellow',
+    padding: 20,
+    justifyContent: 'flex-end',
+  }
+});
