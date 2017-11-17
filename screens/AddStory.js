680d4d5cf24c6003ddc2102dc5885a19ded81ca0
Ega Radiegtya <radiegtya@Egas-MacBook-Pro.local>
2017-11-17 07:49:38 +0700

    14.day8-imagepicker

--- screens/AddStory.js
+++ screens/AddStory.js
@@ -1,13 +1,55 @@
 import React, {Component} from 'react';
-import {Container, Content, Text} from 'native-base';
+import {Container, Content, Text, Button} from 'native-base';
+import ImagePicker from 'react-native-image-picker';
 
-export default class Search extends Component{
+export default class AddStory extends Component{
+
+  state = {
+    source: null
+  }
+
+  handleAddMedia(){
+    var options = {
+      title: 'Select Media',
+      storageOptions: {
+        skipBackup: true,
+        path: 'images'
+      }
+    };
+
+
+    ImagePicker.showImagePicker(options, (response) => {
+      console.log('Response = ', response);
+
+      if (response.didCancel) {
+        console.log('User cancelled image picker');
+      }
+      else if (response.error) {
+        console.log('ImagePicker Error: ', response.error);
+      }
+      else if (response.customButton) {
+        console.log('User tapped custom button: ', response.customButton);
+      }
+      else {
+        let source = { uri: response.uri };
+
+        // You can also display the image using data:
+        // let source = { uri: 'data:image/jpeg;base64,' + response.data };
+
+        this.setState({
+          source: source
+        });
+      }
+    });
+  }
 
   render(){
     return(
       <Container>
         <Content>
-          <Text>AddStory Page</Text>
+          <Button onPress={()=> this.handleAddMedia()}>
+            <Text>Add Media</Text>
+          </Button>
         </Content>
       </Container>
     )
